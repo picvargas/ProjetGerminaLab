25a90fa75eafa8c277d22b56054dcccf5024b2c0
Murilo Soares <murilo.soares@picpay.com>
2025-04-23 18:18:08 -0300

    comeco navbar

---
+++ ProjetoGerminalab/src/app/menu-vertical/menu-vertical.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-vertical',
+  imports: [],
+  templateUrl: './menu-vertical.component.html',
+  styleUrl: './menu-vertical.component.css'
+})
+export class MenuVerticalComponent {
+
+}
