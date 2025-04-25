16a5a150901bea704af93f302676b2db5a4b857c
Murilo Soares <murilo.soares@picpay.com>
2025-04-25 15:35:08 -0300

    Arrumando login

--- ProjetoGerminalab/src/app/app.component.ts
+++ ProjetoGerminalab/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { MenuVerticalComponent } from "./menu-vertical/menu-vertical.component";
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, MenuVerticalComponent],
+  imports: [RouterOutlet, NavbarComponent, MenuVerticalComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
